082c372c5e3cb84ef83995b3eb12b28f59c6e65e
“6qiongtao” <6qiongtao@163.com>
2023-01-31 14:28:55 +0800

    1.单机docker-compose

---
+++ single/jaeger.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Hello"
+JAEGER=$(docker ps -a|grep jaeger)
+echo $JAEGER
+if  [ ! -n "$JAEGER" ];then #is null
+    echo "docker run jaeger" 
+    docker run -d --name jaeger \
+    -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 \
+    -p 5775:5775/udp \
+    -p 6831:6831/udp \
+    -p 6832:6832/udp \
+    -p 5778:5778 \
+    -p 16686:16686 \
+    -p 14268:14268 \
+    -p 9411:9411 \
+    jaegertracing/all-in-one:1.18
+else
+    STATUS=$(docker ps -a |grep jaeger|grep Up)
+    echo $STATUS
+    if [  -n "$STATUS" ];then #not null
+        echo "jaeger is running"
+    else
+        echo "jaeger is not running"
+        docker restart jaeger
+    fi
+fi
+
